faff79f7a4c364203379050ede46d362a014bd60
seromaho <Sebastian.Hoeft@drheuer.de>
2022-09-01 12:31:12 +0200

    removed unused source code

--- Extensions/String_Extensions.cs
+++ Extensions/String_Extensions.cs
@@ -4,27 +4,6 @@ namespace Extensions
 {
     public static class String_Extensions
     {
-        //public static string CapitalizeFirstLetter(this string inputString)
-        //{
-        //    int charIndex = 0;
-        //    // Create a char array from input string
-        //    char[] charArray = new char[inputString.Length];
-        //    foreach (char zeichen in inputString)
-        //    {
-        //        charArray[charIndex] = zeichen;
-        //        charIndex++;
-        //    }
-
-        //    // Convert the first char of the char array to be upper case
-        //    string firstLetter = new string(charArray[0], 1);
-        //    firstLetter = firstLetter.ToUpper();
-        //    foreach (char zeichen in firstLetter) { charArray[0] = zeichen; }
-
-        //    // Create an input string out of the char array
-        //    string outputString = new string(charArray);
-        //    return outputString;
-        //}
-
         public static string CapitalizeFirstLetter(this string inputString)
         {
             int charIndex = 0;
